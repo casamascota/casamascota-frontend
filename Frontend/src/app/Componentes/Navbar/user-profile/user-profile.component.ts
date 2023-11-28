11ac62c97d356f057bd4096fd6b1dd8b5a9cf303
CarlosFULLHD <cninareynaga@gmail.com>
2023-11-27 22:22:25 -0400

    añadiendo login SPA v2

--- Frontend/src/app/Componentes/Navbar/user-profile/user-profile.component.ts
+++ Frontend/src/app/Componentes/Navbar/user-profile/user-profile.component.ts
@@ -4,10 +4,24 @@ import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-user-profile',
   template: `
-    <ul *ngIf="auth.user$ | async as user">
-      <li>{{ user.name }}</li>
-      <li>{{ user.email }}</li>
-    </ul>`
+    <div *ngIf="auth.user$ | async as user" class="user-profile">
+      <div class="d-flex align-items-center">
+        <img [src]="user.picture" class="rounded-circle mr-3" style="width: 40px; height: 40px;">
+        <div>
+          <div class="font-weight-bold">{{ user.name }}</div>
+          <div class="text-muted"><small>{{ user.email }}</small></div>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .user-profile {
+      padding: 10px;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      background-color: #f8f9fa;
+    }
+  `]
 })
 export class UserProfileComponent {
   constructor(public auth: AuthService) {}
