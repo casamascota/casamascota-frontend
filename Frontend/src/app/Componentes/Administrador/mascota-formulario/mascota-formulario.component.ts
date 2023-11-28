36bfd7ee12cfc9466ce72ffbff76ee576601c8ac
CarlosFULLHD <cninareynaga@gmail.com>
2023-11-28 15:38:56 -0400

    uniendo front y back, creacion de mascotas correcto

--- Frontend/src/app/Componentes/Administrador/mascota-formulario/mascota-formulario.component.ts
+++ Frontend/src/app/Componentes/Administrador/mascota-formulario/mascota-formulario.component.ts
@@ -9,10 +9,17 @@ import { MAT_DATE_FORMATS } from '@angular/material/core';
   styleUrls: ['./mascota-formulario.component.css'],
 })
 export class MascotaFormularioComponent {
+  ngOnInit(): void {
+    this.cargarUsuarios();
+
+  }
+  usuarios: any[] = [];
+  tieneDueno: boolean = false;
   formMascota: FormGroup;
   genero: any = ['Macho', 'Hembra'];
   URL_BASE = 'http://localhost:8080/api/v1/';
   maxFechaNacimiento: string;
+  especies: string[] = ['Perro', 'Gato', 'Pez','Ave','Conejo'];
   // id_mascota: number = 0;
   // email: string = "";
   // mensaje: string = "";
@@ -25,17 +32,32 @@ export class MascotaFormularioComponent {
     this.maxFechaNacimiento = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
 
     this.formMascota = this.formBuilder.group({
-      nombre: ['',Validators.required],
-      raza: ['',Validators.required],
-      edad: ['',Validators.required], 
-      genero : ['',Validators.required],
-      fecha_nacimiento: ['',Validators.required],
-      peso: ['',Validators.required],
-      isAdopted: [0,Validators.required],
-      Owner_id_owner: ['',Validators.required],
+      nombre: ['', Validators.required],
+      raza: ['', Validators.required],
+      genero: ['', Validators.required],
+      fecha_nacimiento: ['', Validators.required],
+      especie: ['', Validators.required],
+      peso: ['', Validators.required],
+      isAdopted: [false],  // Cambiado a false por defecto
+      Owner_id_owner: ['']  // Hacemos este campo opcional
     });
   }
+  cargarUsuarios() {
+    const urlUsuarios = this.URL_BASE + 'usuarios/';
+    this.httpClient.get(urlUsuarios).subscribe(
+      (data: any) => {
+        this.usuarios = data;
+      },
+      err => console.log(err)
+    );
+  }
 
+  onTieneDuenoChange() {
+    this.tieneDueno = !this.tieneDueno;
+    if (!this.tieneDueno) {
+      this.formMascota.get('Owner_id_owner')?.setValue(null);
+    }
+  }
   onSubmit() {
     console.log(this.formMascota.value);
     this.guardarMascota();
@@ -43,24 +65,36 @@ export class MascotaFormularioComponent {
 
   guardarMascota() {
     if (this.formMascota.valid) {
-      const url = this.URL_BASE + 'mascota';
       const formData = this.formMascota.value;
+// Establecer un dueño por defecto (ID 1) si no está marcado como adoptado
+const ownerId = formData.isAdopted ? formData.Owner_id_owner : '1';
 
-      this.httpClient.post(url, formData).subscribe(
+const mascotaData = {
+  nombre: formData.nombre,
+  raza: formData.raza,
+  genero: formData.genero,
+  fecha_nacimiento: formData.fecha_nacimiento,
+  peso: formData.peso,
+  especie: formData.especie,
+  usuario: { id_usuario: ownerId },
+  enadopcion: !formData.isAdopted
+};
+  
+      const url = this.URL_BASE + 'mascota/';
+      this.httpClient.post(url, mascotaData).subscribe(
         res => {
           console.log(res);
-          alert('Mascota guardada con exito');
+          alert('Mascota guardada con éxito');
         },
         err => {
-          console.log(err);
+          console.error('Error al guardar la mascota:', err);
         }
-      )
-      
-      console.log(this.formMascota.value);
+      );
+    } else {
+      console.error('Formulario no válido:', this.formMascota.value);
     }
-    
   }
-
+  
 
 
   
